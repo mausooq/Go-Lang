8213e9ae310a4c159365e76290afd09de9c03332
mausooq <mauchoomausooq051@gmail.com>
2024-08-27 08:32:24 +0530

    array

---
+++ go2/variable.go
@@ -0,0 +1,45 @@
+package main
+import ("fmt")
+
+// var x = "mausooq"
+
+func main(){
+	const x int = 3
+
+	// var str string = "mausooq"
+	// fmt.Println(str)
+	// var str2 = "mausooq111"
+	// fmt.Println(str2)
+	// x := "majju"
+	// fmt.Println(x)
+
+	// var a string
+	// var b int 
+	// var c bool
+
+	// fmt.Println(a)
+	// fmt.Println(b)
+	// fmt.Println(c)
+
+
+	// var name string 
+	// name = "maus"
+	// fmt.Print(name)
+
+	// var a,b int = 1,2
+	// fmt.Println(a)
+	// fmt.Println(b)
+
+	// x,y := 3 , "new"
+	// fmt.Println(x,y)
+	// fmt.Println(y)
+
+	// x = 9 
+	// fmt.Print(x)
+
+	var i string = "Hello"
+	var j int = 15
+  
+	fmt.Printf("i has value: %v and type: %T\n", i, i)
+	fmt.Printf("j has value: %v and type: %T", j, j)
+}
---
+++ go3/array.go
@@ -0,0 +1,28 @@
+package main
+import ("fmt")
+
+
+func main() {
+	// var arr1 = [4]int{1, 2, 3, 4}
+	// arr2 := [3]string{"ppp","pppppp","ppppppppp"}
+
+	// var arr1 = [...]int{1, 2, 3, 4}
+	// arr2 := [...]string{"ppp","pppppp","ppppppppp"}
+
+	
+	// fmt.Println(arr1)
+	// fmt.Println(arr2)
+
+
+  prices := [3]int{10,20,30}
+
+//   fmt.Println(prices[0])
+//   fmt.Println(prices[2])
+
+	// prices[2] = 50
+  	// fmt.Println(prices)
+
+  	fmt.Println(len(prices))
+
+}
+
